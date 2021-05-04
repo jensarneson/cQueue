54cb80b174d11373595fa751fc08498422b8045c
Guy Gastineau <strings.stringsandstrings@gmail.com>
2021-05-03 23:48:08 -0400

    Rough draft implementation

--- src/queue.c
+++ src/queue.c
@@ -1,6 +1,82 @@
 #include "queue.h"
 
-const char *hello()
+#include <stdlib.h>
+
+typedef struct QNode {
+  struct QNode *next;
+  QData data;
+} QNode;
+
+struct Queue {
+  QNode *front;
+  QNode *back;
+};
+
+Queue *mkQueue()
 {
-  return "Hello, World!";
+  return calloc(1, sizeof(Queue));
+}
+
+static QNode *mkNode(QData data)
+{
+  QNode *qn = calloc(1, sizeof(*qn));
+  if (qn) {
+    qn->data = data;
+  }
+  return qn;
+}
+
+int qAdd(Queue *q, QData data)
+{
+  if (!q) {
+    return -1;
+  }
+  QNode *new = mkNode(data);
+  // The queue is empty.
+  if (!q->front)
+  {
+    q->front = new;
+  }
+  else if (!q->back)
+  {
+    q->front->next = new;
+    q->back = new;
+  }
+  else
+  {
+    q->back->next = new;
+    q->back = new;
+  }
+  return 0;
+}
+
+QResult qPeek(Queue *q)
+{
+  if (!q || !q->front) {
+    return (QResult){ false, { 0 } };
+  }
+  return (QResult){ true, { q->front->data } };
+}
+
+QResult qNext(Queue *q)
+{
+  if (!q || !q->front) {
+    return (QResult){ false, { 0 } };
+  }
+  QNode *qn = q->front;
+  q->front = qn->next;
+  if (q->front == q->back) {
+    q->back = NULL;
+  }
+  QResult result = { true, { qn->data } };
+  free(qn);
+  return result;
+}
+
+void qDestroy(Queue *q, void (*kill)(QData))
+{
+  QResult result;
+  while ((result = qNext(q)).hasData) {
+    kill(result.payload.data);
+  }
 }
--- src/queue.h
+++ src/queue.h
@@ -1,6 +1,28 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-const char *hello();
+#include <stdbool.h>
+
+typedef int QData;
+
+typedef struct QResult {
+  bool hasData;
+  union {
+    QData data;
+    void *none;
+  } payload;
+} QResult;
+
+typedef struct Queue Queue;
+
+Queue *mkQueue(void);
+
+int qAdd(Queue *q, QData data);
+
+QResult qPeek(Queue *q);
+
+QResult qNext(Queue *q);
+
+void qDestroy(Queue *q, void (*kill)(QData));
 
 #endif // QUEUE_H
